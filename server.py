93d634d006528d31c7c15336d048fbc58aad4120
Guy Kisel <guykisel@users.noreply.github.com>
2016-05-08 15:01:25 -0700

    [WIP] pull vars out of webhook POST data (#2)
    * pull vars out of webhook POST data
    
    * add gemfile and package.json
    
    * implement git cloning and running
    
    * use subprocess.call
    
    * do a shallow git clone
    
    * always delete temp dir

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from __future__ import unicode_literals
 
 import os
+import shutil
+import subprocess
+import tempfile
+import time
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -11,6 +17,52 @@ app = Flask(__name__)
 def root():
     if request.method == 'GET':
         return 'https://github.com/guykisel/inline-plz-bot'
+
+    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
+    data = request.get_json()
+    pull_request = data['pull_request']['number']
+    repo_slug = data['repository']['full_name']
+    name = data['repository']['name']
+    token = os.environ.get('TOKEN')
+    interface = 'github'
+    url = os.environ.get('URL', 'https://github.com')
+
+    branch = data['pull_request']['head']['ref']
+    pull_request_slug = data['pull_request']['head']['repo']['full_name']
+    clone_url = data['pull_request']['head']['repo']['clone_url']
+
+    # make temp dir
+    tempdir = tempfile.mkdtemp()
+    time.sleep(1)
+
+    try:
+        # git clone into temp dir
+        subprocess.call(
+            ['git', 'clone', '--depth', '1', clone_url + '#' + branch],
+            cwd=tempdir
+        )
+        time.sleep(1)
+
+        # run inline-plz in temp dir
+        subprocess.call(
+            [
+                'inline-plz',
+                '--autorun',
+                '--repo-slug={}'.format(repo_slug),
+                '--pull-request={}'.format(pull_request),
+                '--url={}'.format(url),
+                '--token={}'.format(token),
+                '--interface={}'.format(interface),
+                '--zero-exit'
+            ],
+            cwd=os.path.join(tempdir, name)
+        )
+        time.sleep(1)
+    finally:
+        # delete temp dir
+        shutil.rmtree(tempdir)
+        time.sleep(1)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
