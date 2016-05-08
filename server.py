6c8db89991b39be42a4022d4ee554fe5b48749f5
Guy Kisel <guy.kisel@gmail.com>
2016-05-07 19:43:47 -0700

    implement git cloning and running

--- server.py
+++ server.py
@@ -2,8 +2,10 @@
 from __future__ import absolute_import
 
 import os
+import shutil
 import subprocess
 import tempfile
+import time
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -18,6 +20,7 @@ def root():
     data = request.get_json()
     pull_request = data['pull_request']['number']
     repo_slug = data['repository']['full_name']
+    name = data['repository']['name']
     token = os.environ.get('TOKEN')
     interface = 'github'
     url = os.environ.get('URL', 'https://github.com')
@@ -30,12 +33,34 @@ def root():
 
     # make temp dir
     tempdir = tempfile.mkdtemp()
+    time.sleep(1)
 
     # git clone into temp dir
+    subprocess.check_output(
+        ['git', 'clone', clone_url],
+        cwd=tempdir
+    ).strip().decode('utf-8', errors='replace')
+    time.sleep(1)
 
     # run inline-plz in temp dir
+    subprocess.check_output(
+        [
+            'inline-plz',
+            '--autorun',
+            '--repo-slug={}'.format(repo_slug),
+            '--pull-request={}'.format(pull_request),
+            '--url={}'.format(url),
+            '--token={}'.format(token),
+            '--interface={}'.format(interface),
+            '--zero-exit'
+        ],
+        cwd=os.path.join(tempdir, name)
+    ).strip().decode('utf-8', errors='replace')
+    time.sleep(1)
 
     # delete temp dir
+    shutil.rmtree(tempdir)
+    time.sleep(1)
 
 
 if __name__ == '__main__':
